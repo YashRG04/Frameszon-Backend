dd7db8756bfeb145ed9dc3d9bc83ebb1e3297403
yash96499 <67315706+yash96499@users.noreply.github.com>
2023-02-10 09:27:04 +0530

    Update errorhander.js

--- backend/utils/errorhander.js
+++ backend/utils/errorhander.js
@@ -1,4 +1,4 @@
-class ErrorHander extends Error{
+class ErrorHandler extends Error{
     constructor(message,statusCode){
         super(message);
         this.statusCode=statusCode
@@ -10,4 +10,4 @@ class ErrorHander extends Error{
 }
 
 
-module.exports=ErrorHander
+module.exports=ErrorHandler
