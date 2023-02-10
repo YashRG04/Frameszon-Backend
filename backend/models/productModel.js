3f82a2482ffa06739db3a2558794f69413573bfe
yash96499 <67315706+yash96499@users.noreply.github.com>
2023-02-10 09:30:56 +0530

    Update productModel.js

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -14,7 +14,7 @@ const productSchema = mongoose.Schema({
         required:[true,"Please Enter product Price"],
         maxLength:[8,"Price cannot exceed 8 characters"],
     },
-    rating:{
+    ratings:{
         type:Number,
         default:0
     },
@@ -50,6 +50,11 @@ const productSchema = mongoose.Schema({
     },
     reviews:[
         {
+            user: {
+                type: mongoose.Schema.ObjectId,
+                ref: "User",
+                required: true,
+            },
             name:{
                 type:String,
                 required:true,
@@ -64,6 +69,11 @@ const productSchema = mongoose.Schema({
             },
         },
     ],
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+    },
     createdAt:{
         type:Date,
         default:Date.now,
