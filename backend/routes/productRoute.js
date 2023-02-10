6510a786b0575db6cadb0161bcaa4152225c800e
yash96499 <67315706+yash96499@users.noreply.github.com>
2023-02-10 09:29:09 +0530

    Update productRoute.js

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,12 +1,34 @@
 const express =require("express");
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
-const router=express.Router();
+const { 
+    getAllProducts, 
+    createProduct, 
+    updateProduct, 
+    deleteProduct, 
+    getProductDetails,
+    createProductReview,
+    getProductReviews,
+    deleteReview, 
+} = require("../controllers/productController");
 
+const router=express.Router();
 
 router.route("/products").get(getAllProducts);
 
 router.route("/product/new").post(createProduct);
 
-router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
+router
+    .route("/product/:id")
+    .put(updateProduct)
+    .delete(deleteProduct)
+    .get(getProductDetails);
+
+router.route("/product/:id").get(getProductDetails);
+
+router.route("/reviews").put(createProductReview);
+
+router
+    .route("/reviews")
+    .get(getProductReviews)
+    .delete(deleteReview);
 
 module.exports=router;
