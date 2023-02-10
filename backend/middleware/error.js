7d0ecf24ab91bde90d32188149d2e3cf739abffd
yash96499 <67315706+yash96499@users.noreply.github.com>
2023-02-10 09:33:01 +0530

    Update error.js

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -11,6 +11,13 @@ module.exports=(err,req,res,next)=>{
         err = new ErrorHandler(message,400);
     }
 
+    //mongoose duplicate error
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+        err = new ErrorHandler(message,400);
+    }
+
+    
 
     res.status(err.statusCode).json({
         success:false,
